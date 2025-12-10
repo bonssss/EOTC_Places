65e58e9d086ea17e825a97472db70704a3e70170
unknown <bons6710hos@gmail.com>
2025-12-10 21:17:07 +0300

    requiremets file

--- eotc_places/eotc_places/settings.py
+++ eotc_places/eotc_places/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -17,6 +18,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import os
 import cloudinary
 from dotenv import load_dotenv
+import environ
+
+
+env = environ.Env()
+environ.Env.read_env()
+
 
 load_dotenv()
 
@@ -31,9 +38,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&_$zst%4+%&o*si*(7^bzi#p=@dt$*!w&wt8qk+p6jgf6$&mlq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -89,25 +96,30 @@ WSGI_APPLICATION = 'eotc_places.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': os.getenv('DB_NAME'),
-        # 'USER': os.getenv('DB_USER'),
-        # 'PASSWORD': os.getenv('DB_PASSWORD'),
-        # 'HOST': os.getenv('DB_HOST'),
-        # 'PORT': os.getenv('DB_PORT'),
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#         # 'NAME': BASE_DIR / 'db.sqlite3',
+#         # 'ENGINE': 'django.db.backends.mysql',
+#         # 'NAME': os.getenv('DB_NAME'),
+#         # 'USER': os.getenv('DB_USER'),
+#         # 'PASSWORD': os.getenv('DB_PASSWORD'),
+#         # 'HOST': os.getenv('DB_HOST'),
+#         # 'PORT': os.getenv('DB_PORT'),
+#         # 'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+# render database
+import dj_database_url
+DATABASES= {
+    'default':dj_database_url.parse(env('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
